05fb6c5320a9c141d261da15971c7fa82b081da5
K1516 <k1516@student.jamk.fi>
2016-01-19 12:31:44 +0200

    Adding some homeworks

---
+++ DEMO3/Harj5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student();
+
+            students[0].Name = "Risto";
+            students[0].Age = 21;
+            students[0].Hometown = "Rauma";
+            students[0].PrintData();
+
+            students[1] = new Student(); 
+            students[1].Name = "Ossi";
+            students[1].Age = 10;
+            students[1].Hometown = "Azerbaizan";
+            students[1].PrintData();
+
+            students[2] = new Student();
+            students[2].Name = "Peppi";
+            students[2].Age = 30;
+            students[2].Hometown = "Toijala";
+            students[2].PrintData();
+
+            students[3] = new Student();
+            students[3].Name = "Niki";
+            students[3].Age = 25;
+            students[3].Hometown = "Jyväskylä";
+            students[3].PrintData();
+
+            students[4] = new Student();
+            students[4].Name = "Anna";
+            students[4].Age = 18;
+            students[4].Hometown = "Oulu";
+            students[4].PrintData();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DEMO3/Harj5/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj5
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Hometown { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Student info");
+            Console.WriteLine("- Name = " + Name);
+            Console.WriteLine("- Age = " + Age);
+            Console.WriteLine("- Hometown = " + Hometown);
+
+        }
+    }
+}

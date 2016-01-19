b310c7d618a43d5688940423fc173fd113de5545
K1516 <k1516@student.jamk.fi>
2016-01-19 12:13:13 +0200

    adding homeworks

---
+++ DEMO3/Harj4/CAR.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj4
+{
+    class CAR
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Car Data: ");
+            Console.WriteLine("- Name = " + Name);
+            Console.WriteLine("- Speed = " + Speed + "km/h");
+            Console.WriteLine("- Tyres = " + Tyres + "'");
+
+        }
+        public override string ToString()
+        {
+            return "Name : " + Name + " Speed : " + Speed + " Tyres : " + Tyres;
+        }
+    }
+}
---
+++ DEMO3/Harj4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CAR car1 = new CAR();
+            car1.Name = "Lada";
+            car1.Speed = 90;
+            car1.Tyres = 13;
+            car1.PrintData();
+            Console.WriteLine(car1.ToString());
+
+            CAR car2 = new CAR();
+            car2.Name = "Mercedes";
+            car2.Speed = 250;
+            car2.Tyres = 19;
+            car2.PrintData();
+            Console.WriteLine(car2.ToString());
+            Console.ReadLine();
+        }
+    }
+}

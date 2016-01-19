94b3968470e4df350278ebb367ec9fd4138413e6
K1516 <k1516@student.jamk.fi>
2016-01-19 13:37:44 +0200

    finishing my homeworks

---
+++ DEMO3/Harj6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stick stick = new Stick();
+            stick.Model = "Exel";
+            stick.Flex = 2.5;
+            stick.Height = 110;
+            stick.Colour = "Red";
+            stick.PrintData();
+            Console.WriteLine(stick.ToString());
+
+            Stick stick2 = new Stick();
+            stick2.Model = "FatPipe";
+            stick2.Flex = 3.0;
+            stick2.Height = 95;
+            stick2.Colour = "Black";
+            stick2.PrintData();
+            Console.WriteLine(stick2.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DEMO3/Harj6/Stick.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj6
+{
+    class Stick
+    {
+        public string Model { get; set; }
+        public double Flex { get; set; }
+        public int Height { get; set; }
+        public string Colour { get; set; }
+        public void PrintData()
+        {
+            Console.WriteLine("Stick Data");
+            Console.WriteLine("- Model = " + Model);
+            Console.WriteLine("- Flex = " + Flex);
+            Console.WriteLine("- Height = " + Height);
+            Console.WriteLine("- Grip Colour = " + Colour);
+        }
+        public override string ToString()
+        {
+            return "Model : " + Model + " Flex : " + Flex + " Height : " + Height + " Colour : " + Colour;
+        }
+    }
+}
